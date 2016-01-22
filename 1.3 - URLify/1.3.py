2507ac80a07bf6106d26170c30ee7ec5f6454285
austinthiel <austin@angrydrop.net>
2016-01-21 16:20:08 -0800

    fixed tabs on comments

--- 1.3 - URLify/1.3.py
+++ 1.3 - URLify/1.3.py
@@ -3,10 +3,10 @@
 # (Note: if implementing in Java, please use a character array so that you can perform this operation in place.)
 
 def URLify(str, size):
-	str = str[:size]					# cut off excess spaces in the string
-	str = str.replace(" ", "%20")		# replace remaining spaces with '%20'
+	str = str[:size]				# cut off excess spaces in the string
+	str = str.replace(" ", "%20")			# replace remaining spaces with '%20'
 	return str
 
 str1 = "Mr John Smith    "
-size1 = 13								# this is the length of characters to act on, not length of the whole string
+size1 = 13						# this is the length of characters to act on, not length of the whole string
 print(URLify(str1, size1))
