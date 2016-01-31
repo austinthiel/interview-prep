7b950cab5eeb7db20485985cf56de77dadb8a0bf
Austin Thiel <austin@angrydrop.net>
2016-01-30 18:28:01 -0800

    Update 1.8.py

--- 1.8 - Zero Matrix/1.8.py
+++ 1.8 - Zero Matrix/1.8.py
@@ -8,9 +8,9 @@ def zeroMatrix(A):
 	for j in range(cols):
 		for i in range(rows):
 			if A[i][j] == 0:
-				if i not in rowStor:		# avoid storing duplicate rows
+				if i not in rowStor:			# avoid storing duplicate rows
 					rowStor.append(i)		# store the row position to zero out later
-				if j not in colStor:		# avoid storing duplicate columns
+				if j not in colStor:			# avoid storing duplicate columns
 					colStor.append(j)		# same with the column position in a separate array
 	for row in rowStor:
 		for j in range(cols):
@@ -20,5 +20,5 @@ def zeroMatrix(A):
 			A[i][col] = 0;					# and do the same with the columns
 	return A;
 
-A1 = [[1,0,1,0], [2,2,0,2], [3,3,3,3]]		# Test Case 1 ([[0,0,0,0],[0,0,0,0],[3,0,0,0]])
+A1 = [[1,0,1,0], [2,2,0,2], [3,3,3,3]]					# Test Case 1 ([[0,0,0,0],[0,0,0,0],[3,0,0,0]])
 print(zeroMatrix(A1))
