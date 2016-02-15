2cd6cbcf1e598cc1e6cac6e485c5a6832618f27e
Austin Thiel <austin@angrydrop.net>
2016-02-15 09:46:29 -0800

    Update 2.2.py

--- 2.2 - Return Kth to Last/2.2.py
+++ 2.2 - Return Kth to Last/2.2.py
@@ -3,22 +3,22 @@
 
 import LinkedList
 
-def returnKthToLast(head, k):						# no need to take in the whole linked list, we just need the head
-	if head == None:								# recurse when the head is a Nonetype (end of the list)
+def returnKthToLast(head, k):					# no need to take in the whole linked list, we just need the head
+	if head == None:					# recurse when the head is a Nonetype (end of the list)
 		return 0
 	count = returnKthToLast(head.next, k) + 1		# increment and go deeper!
 	if count == k:
 		print(head.data)
 	return count
 
-myList = LinkedList.UnorderedList()					# Test Case 1
+myList = LinkedList.UnorderedList()				# Test Case 1
 myList.add(5)
 myList.add(3)
 myList.add(10)
 myList.add(2)
 myList.add(1)
 
-k = 3												# 3rd from last element is 10
+k = 3								# 3rd from last element is 10
 
 returnKthToLast(myList.head, k)
 
